d0a0bab17817c5c576cae86a97a8b38a4717d5c4
Vivek Gupta <40853271+VivekkkGupta@users.noreply.github.com>
2024-12-28 18:07:48 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PageLayout from './pages/PageLayout'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      element:<PageLayout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        }
+      ]
+    }
+  ])
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
